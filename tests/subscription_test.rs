d93212728778959bfb79600d9eec4a10f541f3a3
8ball030 <8baller@station.codes>
2026-02-06 14:15:23 +0000

    added-subscription-test

---
+++ tests/subscription_test.rs
@@ -0,0 +1,41 @@
+use log::{Level::Info, info};
+use simple_logger::init_with_level;
+use thalex_rust_sdk::{
+    types::{Environment, ExternalEvent},
+    ws_client::WsClient,
+};
+
+#[tokio::test]
+async fn instruments_sub() -> Result<(), Box<dyn std::error::Error>> {
+    init_with_level(Info).unwrap();
+
+    let client = WsClient::new_public(Environment::Mainnet).await.unwrap();
+    let (tx, rx) = tokio::sync::oneshot::channel();
+    let tx_arc = std::sync::Arc::new(tokio::sync::Mutex::new(Some(tx)));
+    client.wait_for_connection().await;
+
+    let _ = client
+        .subscriptions()
+        .market_data()
+        .instruments(move |msg| {
+            // Parses into a json value initally
+            let tx = tx_arc.clone();
+            async move {
+                info!("Got an instruments update");
+                let _ = tx.lock().await.take().map(|tx| tx.send(()));
+            }
+        })
+        .await;
+
+    // wait for 20 seconds or until we receive the signal to exit
+    tokio::select! {
+        _ = rx => {
+            info!("Received signal to exit after receiving the first update");
+        }
+        _ = tokio::time::sleep(std::time::Duration::from_secs(20)) => {
+            info!("Timeout reached without receiving an update");
+        }
+    }
+    Ok(())
+}
+
